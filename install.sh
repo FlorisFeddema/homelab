888102e08f38f2aa66b86c937787c0f4be9e5abc
Floris Feddema <Floris1996@hotmail.com>
2023-05-09 23:28:08 +0200

    update values

--- install.sh
+++ install.sh
@@ -14,12 +14,10 @@ applyFile=$basePath/apply.yaml
 files=$(find $basePath -type f | grep ".yaml" | grep -v "application.yaml" | grep -v "values.yaml" | grep -v "namespace.yaml")
 
 namespace=$(yq '.spec.destination.namespace' $applicationFile )
-chart=$(yq '.spec.source.chart' $applicationFile )
-repo=$(yq '.spec.source.repoURL' $applicationFile )
-version=$(yq '.spec.source.targetRevision' $applicationFile )
+chart=$(yq '.spec.sources[1].chart' $applicationFile )
+repo=$(yq '.spec.sources[1].repoURL' $applicationFile )
+version=$(yq '.spec.sources[1].targetRevision' $applicationFile )
 name=$(yq '.metadata.name' $applicationFile )
-
-yq '.spec.source.helm.values' $applicationFile > $valuesFile
 
 helm repo add temp $repo
 helm repo update
@@ -39,5 +37,4 @@ echo "*********************"
 eval $command
 
 helm repo remove temp
-rm $valuesFile
 
