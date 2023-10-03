0809e3c5401a5a5f7633ac21eb933ebbc4b25afd
Floris Feddema <Floris1996@hotmail.com>
2023-10-03 15:38:15 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,49 +1,25 @@
 #!/bin/bash
 
-path=$1
-app=$2
+app=$1
+namespace=$2
 
-basePath=$path/$app
-
-if [ -z "$basePath" ]; then
-    echo "no path"
+if [ -z "$app" ]; then
+    echo "no app given"
     exit 1
 fi
 
-applicationFile=$path/$app.yaml
-valuesFile=$basePath/values.yaml
-namespaceFile=$basePath/namespace.yaml
-files=$(find $basePath -type f | grep ".yaml" | grep -v "application.yaml" | grep -v "values.yaml" | grep -v "namespace.yaml")
-
-namespace=$(yq '.spec.destination.namespace' $applicationFile )
-chart=$(yq '.spec.sources[1].chart' $applicationFile )
-repo=$(yq '.spec.sources[1].repoURL' $applicationFile )
-version=$(yq '.spec.sources[1].targetRevision' $applicationFile )
-name=$(yq '.metadata.name' $applicationFile )
-
-helm repo add temp $repo
-helm repo update
-
-kubectl apply -f $namespaceFile
-
-echo "$files" | while read line ; do
-   kubectl apply -f $line
-done
-
-if [[ "$repo" == *"https://"* ]]; then
-    command="helm upgrade --install $name temp/$chart --namespace $namespace --values $valuesFile --version $version"
-else
-    command="helm upgrade --install $name oci://$repo/$chart --namespace $namespace --values $valuesFile --version $version"
+if [ -z "$namespace" ]; then
+    namespace=$app
 fi
+
+basePath=products/$app
+
+helm dependency build $basePath
+
+command="helm upgrade --install $app $basePath --namespace $namespace
 
 echo "*********************"
 echo $command
 echo "*********************"
 
 eval $command
-
-helm repo remove temp
-
-echo "$files" | while read line ; do
-   kubectl apply -f $line
-done
