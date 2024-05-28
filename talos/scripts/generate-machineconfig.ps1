5bbed20e4ba509c400ce8aa0a6e0607d70720f2e
Rouke Broersma <mobrockers@gmail.com>
2024-05-28 22:18:47 +0200

    talos: Improve machineconfig script

--- talos/scripts/generate-machineconfig.ps1
+++ talos/scripts/generate-machineconfig.ps1
@@ -4,6 +4,9 @@ param(
     [boolean]$applyConfig = $false
 )
 
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
 if(-not (Get-Module powershell-yaml -ListAvailable)) {
     Install-Module powershell-yaml -Scope CurrentUser -Force
 }
@@ -16,29 +19,40 @@ for ($node=0; $node -lt $controlPlanes.Count; $node++) {
 
     Write-Host "Generating control plane $node machineconfig"
 
-    $endpoint = "https://$$(controlPlanes[$node]):6443"
-    $output= $applyConfig ? "$repoPath/talos/rendered/control-plane-$node.yaml" : "-"
+    $nodeIp = $controlPlanes[$node]
+    $endpoint = "https://$($nodeIp):6443"
+    $output = $applyConfig ? "$repoPath/talos/rendered/control-plane-$node.yaml" : "-"
 
-    talosctl gen config talos-broersma $endpoint                                                              `
-    --output $output                                                                                          `
-    --output-types controlplane                                                                               `
-    --with-cluster-discovery=false                                                                            `
-    --with-docs=$extendedConfig                                                                               `
-    --with-examples=$extendedConfig                                                                           `
-    --with-secrets $repoPath/secrets.yaml                                                                     `
-    --config-patch @$repoPath/talos/patches/cluster-name.yaml                                                 `
-    --config-patch @$repoPath/talos/patches/cluster-endpoint.yaml                                             `
-    --config-patch @$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml                                   `
-    --config-patch @$repoPath/talos/patches/enable-kubeprism.yaml                                             `
-    --config-patch @$repoPath/talos/patches/region-meijhorst.yaml                                             `
-    --config-patch @$repoPath/talos/patches/zone-embla.yaml                                                   `
-    --config-patch-control-plane @$repoPath/talos/patches/control-plane-vip.yaml                              `
-    --config-patch-control-plane @$repoPath/talos/nodes/control-plane-$node.yaml                              `
-    --kubernetes-version $kubernetesVersion                                                                   `
-    --force
+    $cmdArgList = @(
+        "gen", "config", "talos-broersma", $endpoint,              
+        "--output=$output",
+        "--output-types=controlplane",
+        "--with-cluster-discovery=false",
+        "--with-docs=$extendedConfig",
+        "--with-examples=$extendedConfig",
+        "--with-secrets=$repoPath/secrets.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-name.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-endpoint.yaml",
+        "--config-patch=@$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml",
+        "--config-patch=@$repoPath/talos/patches/enable-kubeprism.yaml",
+        "--config-patch=@$repoPath/talos/patches/region-meijhorst.yaml",
+        "--config-patch=@$repoPath/talos/patches/zone-embla.yaml",
+        "--config-patch-control-plane=@$repoPath/talos/patches/control-plane-vip.yaml",
+        "--config-patch-control-plane=@$repoPath/talos/nodes/control-plane-$node.yaml",
+        "--kubernetes-version=$kubernetesVersion"
+        "--force"
+    )
+
+    &talosctl $cmdArgList
 
     if($applyConfig) {
-        talosctl apply --talosconfig $repoPath/talosconfig --nodes $controlPlanes[$node] --file $repoPath/talos/rendered/control-plane-$node.yaml
+        $cmdArgList = @(
+            "apply",
+            "--talosconfig=$repoPath/talosconfig",
+            "--nodes=$nodeIp",
+            "--file=$repoPath/talos/rendered/control-plane-$node.yaml"
+        )
+        &talosctl $cmdArgList
     }
 }
 
@@ -47,28 +61,38 @@ for ($node=0; $node -lt $intelWorkers.Count; $node++) {
 
     Write-Host "Generating worker $node machineconfig"
 
-    $endpoint = "https://$$(intelWorkers[$node]):6443"
+    $nodeIp = $intelWorkers[$node]
+    $endpoint = "https://$($nodeIp):6443"
     $output = $applyConfig ? "$repoPath/talos/rendered/worker-$node.yaml" : "-"
 
-    talosctl gen config talos-broersma $endpoint                                                       `
-    --output $output                                                                                   `
-    --output-types worker                                                                              `
-    --with-cluster-discovery=false                                                                     `
-    --with-docs=$extendedConfig                                                                        `
-    --with-examples=$extendedConfig                                                                    `
-    --with-secrets $repoPath/secrets.yaml                                                              `
-    --config-patch @$repoPath/talos/patches/cluster-name.yaml                                          `
-    --config-patch @$repoPath/talos/patches/cluster-endpoint.yaml                                      `
-    --config-patch @$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml                            `
-    --config-patch @$repoPath/talos/patches/enable-kubeprism.yaml                                      `
-    --config-patch @$repoPath/talos/patches/region-meijhorst.yaml                                      `
-    --config-patch @$repoPath/talos/patches/zone-embla.yaml                                            `
-    --config-patch-worker @$repoPath/talos/nodes/worker-$node.yaml                                     `
-    --kubernetes-version $kubernetesVersion                                                            `
-    --force
+    $cmdArgList = @(
+        "gen", "config", "talos-broersma", $endpoint,              
+        "--output=$output",
+        "--output-types=worker",
+        "--with-cluster-discovery=false",
+        "--with-docs=$extendedConfig",
+        "--with-examples=$extendedConfig",
+        "--with-secrets=$repoPath/secrets.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-name.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-endpoint.yaml",
+        "--config-patch=@$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml",
+        "--config-patch=@$repoPath/talos/patches/region-meijhorst.yaml",
+        "--config-patch=@$repoPath/talos/patches/zone-embla.yaml",
+        "--config-patch-worker=@$repoPath/talos/nodes/worker-$node.yaml",
+        "--kubernetes-version=$kubernetesVersion"
+        "--force"
+    )
+
+    &talosctl $cmdArgList
 
     if($applyConfig) {
-        talosctl apply --talosconfig $repoPath/talosconfig --nodes $intelWorkers[$node] --file $repoPath/talos/rendered/worker-$node.yaml
+        $cmdArgList = @(
+            "apply",
+            "--talosconfig=$repoPath/talosconfig",
+            "--nodes=$nodeIp",
+            "--file=$repoPath/talos/rendered/worker-$node.yaml"
+        )
+        &talosctl $cmdArgList
     }
 }
 
@@ -77,27 +101,37 @@ for ($node=0; $node -lt $piWorkers.Count; $node++) {
 
     Write-Host "Generating worker pi $node machineconfig"
 
-    $endpoint = "https://$$(piWorkers[$node]):6443"
+    $nodeIp = $piWorkers[$node]
+    $endpoint = "https://$($nodeIp):6443"
     $output = $applyConfig ? "$repoPath/talos/rendered/worker-pi-$node.yaml" : "-"
 
-    talosctl gen config talos-broersma $endpoint                                                          `
-    --output $output                                                                                      `
-    --output-types worker                                                                                 `
-    --with-cluster-discovery=false                                                                        `
-    --with-docs=$extendedConfig                                                                           `
-    --with-examples=$extendedConfig                                                                       `
-    --with-secrets $repoPath/secrets.yaml                                                                 `
-    --config-patch @$repoPath/talos/patches/cluster-name.yaml                                             `
-    --config-patch @$repoPath/talos/patches/cluster-endpoint.yaml                                         `
-    --config-patch @$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml                               `
-    --config-patch @$repoPath/talos/patches/enable-kubeprism.yaml                                         `
-    --config-patch @$repoPath/talos/patches/region-meijhorst.yaml                                         `
-    --config-patch @$repoPath/talos/patches/zone-pi.yaml                                                  `
-    --config-patch-worker @$repoPath/talos/nodes/worker-pi-$node.yaml                                     `
-    --kubernetes-version $kubernetesVersion                                                               `
-    --force
+    $cmdArgList = @(
+        "gen", "config", "talos-broersma", $endpoint,              
+        "--output=$output",
+        "--output-types=worker",
+        "--with-cluster-discovery=false",
+        "--with-docs=$extendedConfig",
+        "--with-examples=$extendedConfig",
+        "--with-secrets=$repoPath/secrets.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-name.yaml",
+        "--config-patch=@$repoPath/talos/patches/cluster-endpoint.yaml",
+        "--config-patch=@$repoPath/talos/patches/disable-cni-and-kube-proxy.yaml",
+        "--config-patch=@$repoPath/talos/patches/region-meijhorst.yaml",
+        "--config-patch=@$repoPath/talos/patches/zone-pi.yaml",
+        "--config-patch-worker=@$repoPath/talos/nodes/worker-pi-$node.yaml",
+        "--kubernetes-version=$kubernetesVersion"
+        "--force"
+    )
+
+    &talosctl $cmdArgList
 
     if($applyConfig) {
-        talosctl apply --talosconfig $repoPath/talosconfig --nodes $piWorkers[$node] --file $repoPath/talos/rendered/worker-pi-$node.yaml
+        $cmdArgList = @(
+            "apply",
+            "--talosconfig=$repoPath/talosconfig",
+            "--nodes=$nodeIp",
+            "--file=$repoPath/talos/rendered/worker-pi-$node.yaml"
+        )
+        &talosctl $cmdArgList
     }
 }
