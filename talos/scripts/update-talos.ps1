019ad4fa191474b5f78d6f2ac8f7bc4be8d0631c
Rouke Broersma <mobrockers@gmail.com>
2024-10-07 20:58:05 +0200

    talos: Upgrade to 1.8.0

--- talos/scripts/update-talos.ps1
+++ talos/scripts/update-talos.ps1
@@ -12,11 +12,11 @@ if(-not (Get-Module powershell-yaml -ListAvailable)) {
 
 $nodes = Get-Content "$repoPath/talos/nodes/nodes.yaml" | ConvertFrom-Yaml
 
-$controlPlaneImage = "factory.talos.dev/installer/e3fab82b561b5e559cdf1c0b1e5950c0e52700b9208a2cfaa5b18454796f3a7e:v1.7.1"
+$controlPlaneImage = "factory.talos.dev/installer/e3fab82b561b5e559cdf1c0b1e5950c0e52700b9208a2cfaa5b18454796f3a7e:v1.8.0"
 
-$piWorkerImage = "factory.talos.dev/installer/f47e6cd2634c7a96988861031bcc4144468a1e3aef82cca4f5b5ca3fffef778a:v1.7.1"
+$piWorkerImage = "factory.talos.dev/installer/f47e6cd2634c7a96988861031bcc4144468a1e3aef82cca4f5b5ca3fffef778a:v1.8.0"
 
-$intelWorkerImage = "factory.talos.dev/installer/930a00fbcce4d3bcd531c92e13d24412df7b676f818004fbbdfeb693e4dcb649:v1.7.1"
+$intelWorkerImage = "factory.talos.dev/installer/930a00fbcce4d3bcd531c92e13d24412df7b676f818004fbbdfeb693e4dcb649:v1.8.0"
 
 if(($($upgradeControlPlane, $upgradeWorkerPi, $upgradeWorkerIntel) | where-object {$_ -eq $true}).Count -gt 1) {
     Write-Host "You must specify at most one upgrade set"
@@ -33,7 +33,7 @@ if($upgradeControlPlane) {
 if($upgradeWorkerPi) {
     foreach($node in $nodes.ips.piWorkers) {
         Write-Host "Upgrading pi worker node $node"
-        talosctl upgrade --talosconfig $talosConfigPath --nodes $node --image $piWorkerImage --wait
+        talosctl upgrade --talosconfig $talosConfigPath --nodes $node --image $piWorkerImage --wait --stage
     }
 }
 
